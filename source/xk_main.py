e8fdb2fd373d80abb0daa5e01e8136a9f450b448
YangSiyuan328 <1017599853@qq.com>
2024-05-11 14:20:46 +0800

    1.fix 解决跨域问题

--- source/xk_main.py
+++ source/xk_main.py
@@ -1,10 +1,11 @@
 from flask import Flask
-
+from flask_cors import CORS
 from source.xk_mainview import MainBlueprint
 
 if __name__ == "__main__":
     """软件入口"""
     app = Flask(__name__, static_folder="static", template_folder="templates")
+    CORS(app)  # 启用 CORS
     app.register_blueprint(MainBlueprint.creator_blueprint())  # 在app中注册蓝图
     print(app.url_map)
     app.run(debug=True)  # 以debug模式运行应用
