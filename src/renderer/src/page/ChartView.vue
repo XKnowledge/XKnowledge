3f1c4ae5f18965e8a7dd745b99f7b109489a5898
FishPotatoChen <FishPotatoChen@163.com>
2024-08-09 19:53:02 +0800

    fix：修改类目bug，修改变量名；feat：支持删除节点，实现连接高亮，实现创建新连接，实现修改连接，实现删除连接 (#6)
    * 修改变量名
    
    * fix：修改类目bug
    
    * feat：支持删除节点
    
    * feat：实现连接高亮，实现创建新连接，实现修改连接，实现删除连接

--- src/renderer/src/page/ChartView.vue
+++ src/renderer/src/page/ChartView.vue
@@ -3,7 +3,9 @@
     <a-layout style="height: 100vh">
       <a-layout-header :style="headerStyle" class="move-show">
         <a-button class="no-move-button" @click="createNode">创建节点</a-button>
-        <a-button class="no-move-button" @click="createEdge">创建边</a-button>
+        <a-button class="no-move-button" @click="deleteNode">删除节点</a-button>
+        <a-button class="no-move-button" @click="createEdge">创建连接</a-button>
+        <a-button class="no-move-button" @click="deleteEdge">删除连接</a-button>
         <a-button class="no-move-button" @click="toggleSider">编辑框</a-button>
         <!-- 控制按钮 -->
       </a-layout-header>
@@ -25,18 +27,14 @@
               <a-textarea v-model:value="newNode.des" />
             </a-form-item>
             <a-form-item label="所属类目">
-              <a-select
-                v-model:value="newNode.category"
-                placeholder="请选择类目"
-                style="width: 200px"
-                :options="items.map(item => ({ value: item }))"
-              >
+              <a-select v-model:value="newNode.category" placeholder="请选择类目" style="width: 200px"
+                        :options="categoryItems.map(item => ({ value: item }))">
                 <template #dropdownRender="{ menuNode: menu }">
                   <v-nodes :vnodes="menu" />
                   <a-divider style="margin: 4px 0" />
                   <a-space style="padding: 4px 8px">
-                    <a-input ref="inputRef" v-model:value="name" placeholder="类目名" />
-                    <a-button type="text" @click="addItem">
+                    <a-input ref="inputRef" v-model:value="categoryName" placeholder="类目名" />
+                    <a-button type="text" @click="addCategory">
                       <template #icon>
                         <plus-outlined />
                       </template>
@@ -47,7 +45,7 @@
               </a-select>
             </a-form-item>
             <a-form-item label="节点大小">
-              <a-input-number v-model:value="currentNode.symbolSize" :min="1" :max="100" />
+              <a-input-number v-model:value="newNode.symbolSize" :min="1" :max="100" />
             </a-form-item>
             <a-form-item>
               <a-button @click="createNodeSubmit">创建节点</a-button>
@@ -62,18 +60,14 @@
               <a-textarea v-model:value="currentNode.des" />
             </a-form-item>
             <a-form-item label="所属类目">
-              <a-select
-                v-model:value="currentNode.category"
-                placeholder="请选择类目"
-                style="width: 200px"
-                :options="items.map(item => ({ value: item }))"
-              >
+              <a-select v-model:value="currentNode.category" placeholder="请选择类目" style="width: 200px"
+                        :options="categoryItems.map(item => ({ value: item }))">
                 <template #dropdownRender="{ menuNode: menu }">
                   <v-nodes :vnodes="menu" />
                   <a-divider style="margin: 4px 0" />
                   <a-space style="padding: 4px 8px">
-                    <a-input ref="inputRef" v-model:value="name" placeholder="类目名" />
-                    <a-button type="text" @click="addItem">
+                    <a-input ref="inputRef" v-model:value="categoryName" placeholder="类目名" />
+                    <a-button type="text" @click="addCategory">
                       <template #icon>
                         <plus-outlined />
                       </template>
@@ -91,9 +85,33 @@
             </a-form-item>
           </a-form>
 
+          <a-form v-show="createEdgeVisible">
+            <a-form-item label="名称">
+              <a-textarea v-model:value="newEdge.name" />
+            </a-form-item>
+            <a-form-item label="描述">
+              <a-textarea v-model:value="newEdge.des" />
+            </a-form-item>
+            <a-form-item>
+              <a-button @click="createEdgeSubmit">创建连接</a-button>
+            </a-form-item>
+          </a-form>
+
+          <a-form v-show="currentEdgeVisible">
+            <a-form-item label="名称">
+              <a-textarea v-model:value="currentEdge.name" />
+            </a-form-item>
+            <a-form-item label="描述">
+              <a-textarea v-model:value="currentEdge.des" />
+            </a-form-item>
+            <a-form-item>
+              <a-button @click="currentEdgeSubmit">修改连接</a-button>
+            </a-form-item>
+          </a-form>
+
         </a-layout-sider>
       </a-layout>
-      <a-layout-footer class="footer-style">Footer</a-layout-footer>
+      <a-layout-footer class="footer-style" v-show=false>Footer</a-layout-footer>
     </a-layout>
   </a-space>
 </template>
@@ -126,30 +144,52 @@ const currentNode = ref({
   "symbolSize": 50,
   "category": ""
 });
-let currentDataIndex;
+let currentNodeDataIndex = -1; // todo 这块有一个优化，可以和highlightNodeList合并，相当于highlightNodeList的最后一个值，不确定能不能替换，替换之后如果highlightNodeList中没有节点，会有问题？
+
+const createEdgeVisible = ref(false);
+const newEdge = ref({
+  "source": "",
+  "target": "",
+  "name": "",
+  "des": ""
+});
+
+const currentEdgeVisible = ref(false);
+const currentEdge = ref({
+  "source": "",
+  "target": "",
+  "name": "",
+  "des": ""
+});
+let currentEdgeDataIndex = -1;
 
 const VNodes = defineComponent({
   props: {
     vnodes: {
       type: Object,
-      required: true,
-    },
+      required: true
+    }
   },
   render() {
     return this.vnodes;
-  },
+  }
 });
 
-let index = 0;
 // 新增时的类目
-const items = ref([]);
+const categoryItems = ref([]);
 const inputRef = ref();
-const name = ref();
-const addItem = e => {
+const categoryName = ref();
+const addCategory = e => {
   e.preventDefault();
-  console.log('addItem');
-  items.value.push(name.value || `New item ${(index += 1)}`);
-  name.value = '';
+  console.log(categoryName.value);
+  if (categoryName.value) {
+    currentNode.value.category = categoryName.value;
+    const pos = categoryItems.value.indexOf(categoryName.value);
+    if (pos === -1) {
+      categoryItems.value.push(categoryName.value);
+    }
+  }
+  categoryName.value = "";
   setTimeout(() => {
     inputRef.value?.focus();
   }, 0);
@@ -159,6 +199,9 @@ const addItem = e => {
 let chartDom = null;
 let chartInstance = null;
 let highlightNodeList = []; // 高亮节点记录
+
+let highlightEdge = null;
+
 let history = []; // 记录历史
 let history_sequence_number = -1; // HSN：历史操作对应的目前的位置
 
@@ -176,6 +219,8 @@ const resetSider = () => {
   errorMessageVisible.value = false;
   createNodeVisible.value = false;
   currentNodeVisible.value = false;
+  createEdgeVisible.value = false;
+  currentEdgeVisible.value = false;
 };
 
 const getChart = (option) => {
@@ -212,7 +257,8 @@ const clickChart = event => {
   if (event.dataType === "node") {
     currentNodeVisible.value = true;
     currentNode.value = event.data;
-    currentDataIndex = event.dataIndex;
+    newNode.value.symbolSize = currentNode.value.symbolSize;
+    currentNodeDataIndex = event.dataIndex;
     console.log(currentNode);
 
     const pos = highlightNodeList.indexOf(event.dataIndex);
@@ -238,6 +284,24 @@ const clickChart = event => {
       }
     }
     console.log(highlightNodeList);
+  } else if (event.dataType === "edge") {
+    currentEdgeVisible.value = true;
+    currentEdge.value = event.data;
+    currentEdgeDataIndex = event.dataIndex;
+
+    if (highlightEdge === null) {
+      highlightEdge = event.dataIndex;
+      operateChart(highlightEdge, "edge", "highlight");
+    } else {
+      if (highlightEdge !== event.dataIndex) {
+        operateChart(highlightEdge, "edge", "downplay");
+        highlightEdge = event.dataIndex;
+        operateChart(highlightEdge, "edge", "highlight");
+      } else {
+        operateChart(highlightEdge, "edge", "downplay");
+        highlightEdge = null;
+      }
+    }
   }
 };
 
@@ -257,6 +321,10 @@ const resizeChart = () => {
   chartInstance.resize();
 };
 
+const refreshChart = () => {
+  chartInstance.setOption(chartData.value);
+};
+
 const toggleSider = () => {
   /**
    * 显示侧边栏
@@ -265,6 +333,12 @@ const toggleSider = () => {
   echartsWidth.value = siderVisible.value ? `calc(100vw - ${270}px)` : "100vw";
   // 使用 nextTick 等待DOM更新完成后执行resize
   nextTick(resizeChart);
+  if (!siderVisible.value) {
+    for (let i = 0; i < highlightNodeList.length; i++) {
+      operateChart(highlightNodeList[i], "node", "downplay");
+    }
+    currentNodeDataIndex = -1;
+  }
 };
 
 const createNode = () => {
@@ -277,6 +351,46 @@ const createNode = () => {
   createNodeVisible.value = true;
   resetError();
   nextTick(resizeChart);
+};
+
+const deleteNode = () => {
+  /**
+   * 删除新节点
+   */
+  resetSider();
+  resetError();
+  if (currentNodeDataIndex >= 0) {
+    const series = chartData.value.series[0];
+
+    const oldName = series.data[currentNodeDataIndex].name;
+
+    // 删除节点
+    let data = [];
+    let length = series.data.length;
+    for (let i = 0; i < length; i++) {
+      if (i !== currentNodeDataIndex) {
+        data.push(series.data[i]);
+      }
+    }
+    chartData.value.series[0].data = data;
+
+    // 删除节点所在的边
+    let links = [];
+    length = series.links.length;
+    for (let i = 0; i < length; i++) {
+      if (series.links[i].source !== oldName && series.links[i].target !== oldName) {
+        links.push(series.links[i]);
+      }
+    }
+    chartData.value.series[0].links = links;
+
+    console.log("chartData");
+    console.log(chartData.value);
+
+    updateLegend();
+    refreshChart();
+  }
+  currentNodeDataIndex = -1;
 };
 
 const resetNewNode = () => {
@@ -316,7 +430,7 @@ const updateLegend = () => {
     return x;
   });
   // 更新选择下拉框类目
-  items.value = categories;
+  categoryItems.value = categories;
 };
 
 const createNodeSubmit = () => {
@@ -329,7 +443,7 @@ const createNodeSubmit = () => {
   if (names.indexOf(newNode.value.name) === -1) {
     chartData.value.series[0].data.push(jsonReactive(newNode.value));
     updateLegend();
-    chartInstance.setOption(chartData.value);
+    refreshChart();
     resetError();
     resetNewNode();
   } else {
@@ -344,14 +458,17 @@ const currentNodeSubmit = () => {
   let names = chartData.value.series[0].data.map((x) => {
     return x.name;
   });
-  const oldName = names[currentDataIndex];
+  const oldName = names[currentNodeDataIndex];
   const newName = currentNode.value.name;
 
   if (oldName !== newName) {
-    names.slice(0, currentDataIndex).push(...names.slice(currentDataIndex + 1)); // 去掉旧节点名称
+    // 修改节点的时候修改了节点名称
+    // names.slice(0, currentNodeDataIndex).push(...names.slice(currentNodeDataIndex + 1)); // 去掉旧节点名称
+    // 思考：为什么不需要去掉旧的节点名称？因为本身就不重名，所以不用去掉
+    // 思考：两个if是否可以合并？不可以合并，因为第二个if还有else分支
     if (names.indexOf(newName) === -1) {
-      chartData.value.series[0].data[currentDataIndex] = jsonReactive(currentNode.value);
-      updateLegend();
+      // 判断修改完的名称是否重名
+      chartData.value.series[0].data[currentNodeDataIndex] = jsonReactive(currentNode.value);
 
       // 修改新节点所在的边
       const links = chartData.value.series[0].links;
@@ -365,21 +482,89 @@ const currentNodeSubmit = () => {
         }
       }
 
-      chartInstance.setOption(chartData.value);
+      updateLegend();
+      refreshChart();
       resetError();
     } else {
       setError("不能创建同名节点");
     }
   } else {
-    chartData.value.series[0].data[currentDataIndex] = jsonReactive(currentNode.value);
+    // 修改节点时没有修改节点名称
+    chartData.value.series[0].data[currentNodeDataIndex] = jsonReactive(currentNode.value);
     updateLegend();
-    chartInstance.setOption(chartData.value);
+    refreshChart();
     resetError();
   }
 };
 
+const resetNewEdge = () => {
+  newEdge.value = {
+    "source": "",
+    "target": "",
+    "name": "",
+    "des": ""
+  };
+};
+
 const createEdge = () => {
-  resizeChart();
+  /**
+   * 创建新连接
+   */
+  resetSider();
+  siderVisible.value = true; // 切换侧边栏的显示状态
+  echartsWidth.value = siderVisible.value ? `calc(100vw - ${270}px)` : "100vw";
+  createEdgeVisible.value = true;
+  resetError();
+  nextTick(resizeChart);
+};
+
+const createEdgeSubmit = () => {
+  /**
+   * 响应创建新连接的提交
+   */
+  if (highlightNodeList.length === 2) {
+    const data = chartData.value.series[0].data;
+    newEdge.value.source = data[highlightNodeList[0]].name;
+    newEdge.value.target = data[highlightNodeList[1]].name;
+    chartData.value.series[0].links.push(jsonReactive(newEdge.value));
+    refreshChart();
+    resetError();
+    resetNewEdge();
+  } else {
+    setError("请选中2个节点");
+  }
+};
+
+const currentEdgeSubmit = () => {
+  /**
+   * 实现连接的动态修改
+   */
+  resetError();
+  chartData.value.series[0].links[currentEdgeDataIndex] = jsonReactive(currentEdge.value);
+  refreshChart();
+};
+
+const deleteEdge = () => {
+  /**
+   * 删除连接
+   */
+  resetSider();
+  resetError();
+  if (currentEdgeDataIndex >= 0) {
+    const series = chartData.value.series[0];
+
+    // 删除连接
+    let links = [];
+    let length = series.links.length;
+    for (let i = 0; i < length; i++) {
+      if (i !== currentEdgeDataIndex) {
+        links.push(series.links[i]);
+      }
+    }
+    chartData.value.series[0].links = links;
+    refreshChart();
+  }
+  currentEdgeDataIndex = -1;
 };
 
 const headerStyle = {
@@ -403,7 +588,7 @@ const echartsStyle = {
   width: "100%",
   height: "100%"
   // width: echartsWidth.value,
-  // height: 'calc(100vh - 86px)'
+  // height: "calc(100vh - 86px)"
 };
 
 </script>
