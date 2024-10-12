99561c707bee9ed55e40867f41542a6e8d62ec50
FishPotatoChen <FishPotatoChen@163.com>
2024-10-12 14:05:18 +0800

    fix：修改示例

--- src/renderer/src/template/template1.ts
+++ src/renderer/src/template/template1.ts
@@ -75,50 +75,44 @@ const createTemplate1 = () => {
         }
       },
       // 数据
-      data: [
+      "data": [
         {
           "name": "node01",
           "des": "nodedes01",
-          "symbolSize": 70,
-          "category": "\u7c7b\u76ee0"
+          "symbolSize": 50,
+          "category": "类目0"
         },
         {
           "name": "node02",
           "des": "nodedes02",
           "symbolSize": 50,
-          "category": "\u7c7b\u76ee1"
+          "category": "类目1"
         },
         {
           "name": "node03",
           "des": "nodedes3",
           "symbolSize": 50,
-          "category": "\u7c7b\u76ee2"
+          "category": "类目2"
         },
         {
           "name": "node05",
           "des": "nodedes05",
           "symbolSize": 50,
-          "category": "\u7c7b\u76ee1"
+          "category": "类目1"
         },
         {
           "name": "node04",
           "des": "nodedes04",
           "symbolSize": 50,
-          "category": "\u7c7b\u76ee3"
+          "category": "类目3"
         }
       ],
-      links: [
+      "links": [
         {
           "source": "node01",
           "target": "node02",
           "name": "link01",
           "des": "link01des"
-        },
-        {
-          "source": "node01",
-          "target": "node03",
-          "name": "link02",
-          "des": "link02des"
         },
         {
           "source": "node01",
@@ -131,6 +125,24 @@ const createTemplate1 = () => {
           "target": "node04",
           "name": "link03",
           "des": "link03des"
+        },
+        {
+          "source": "node03",
+          "target": "node02",
+          "name": "link05",
+          "des": "link05des"
+        },
+        {
+          "source": "node04",
+          "target": "node03",
+          "name": "link02",
+          "des": "link02des"
+        },
+        {
+          "source": "node03",
+          "target": "node05",
+          "name": "link06",
+          "des": "link06des"
         }
       ],
       categories: [
