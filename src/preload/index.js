a88a34fe7bd9d99207efc2d3c029353652fdaf9a
FishPototoChen <FishPotatoChen@163.com>
2026-09-11 16:16:59 +0800

    refactor：删除 legacy act/data 通道，IPC 重构完成

--- src/preload/index.js
+++ src/preload/index.js
@@ -1,21 +1,19 @@
 import { contextBridge, ipcRenderer } from 'electron'
 import { IPC } from '../shared/ipc-channels'
 
+/**
+ * 渲染进程可用的 IPC 接口。
+ * 请求-响应类操作走 invoke（返回 Promise，主进程失败时 reject）；
+ * onRequestClose 是唯一的主进程推送（用户点击窗口关闭按钮）。
+ */
 contextBridge.exposeInMainWorld('electronAPI', {
-  /* 【legacy】Task 8 删除 */
-  receiveAct: (callback) => ipcRenderer.on('act', (_event, data) => callback(data)),
-  sendAct: (act) => ipcRenderer.send('act', act),
-  receiveData: (callback) => ipcRenderer.on('data', (_event, data) => callback(data)),
-  sendData: (data) => ipcRenderer.send('data', data),
-
-  /* 新通道 */
+  openFile: () => ipcRenderer.invoke(IPC.FILE_OPEN),
   saveFile: (payload) => ipcRenderer.invoke(IPC.FILE_SAVE, payload),
   saveFileAs: (payload) => ipcRenderer.invoke(IPC.FILE_SAVE_AS, payload),
-  openFile: () => ipcRenderer.invoke(IPC.FILE_OPEN),
-  closeWindow: () => ipcRenderer.invoke(IPC.APP_CLOSE_WINDOW),
-  enterChartMode: () => ipcRenderer.invoke(IPC.APP_ENTER_CHART_MODE),
   newChartWindow: (payload) => ipcRenderer.invoke(IPC.APP_NEW_CHART_WINDOW, payload),
   takePendingChart: () => ipcRenderer.invoke(IPC.APP_TAKE_PENDING_CHART),
+  enterChartMode: () => ipcRenderer.invoke(IPC.APP_ENTER_CHART_MODE),
+  closeWindow: () => ipcRenderer.invoke(IPC.APP_CLOSE_WINDOW),
   confirmUnsaved: () => ipcRenderer.invoke(IPC.APP_CONFIRM_UNSAVED),
   onRequestClose: (callback) =>
     ipcRenderer.on(IPC.APP_REQUEST_CLOSE, () => callback())
